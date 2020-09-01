b9f406004cde559a3b05cb1c427eb50c78a8678b
Rahel_Hagos <hagos.rahelh@gmail.com>
2020-08-31 20:37:27 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,32 @@
 require 'pry'
 
 class String
-
+  require 'pry'
   def sentence?
+    "HI!".end_with?("!")
+    self.end_with?(".")
+
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
+    a=self.split(/\W+/)
+    a.count
+ end
 
-  end
+
+  def complex_string
+    self.split(/\W+/).count  
+  end 
+ 
 end
